97109dc5d7eaebb4bbce47d087a99f40b7ecf911
roblabla <robinlambertz.dev@gmail.com>
2014-06-01 17:54:19 +0200

    Refactor crackshot and crime hurtPlayer

--- src/main/java/me/ToastHelmi/GrandTheftMinecart/Police/Listener/CrackshotListener.java
+++ src/main/java/me/ToastHelmi/GrandTheftMinecart/Police/Listener/CrackshotListener.java
@@ -9,20 +9,13 @@ import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
 import me.ToastHelmi.GrandTheftMinecart.Police.CrimeManager;
 import me.ToastHelmi.GrandTheftMinecart.StaticValues.StaticMetaDataValue;
 import me.ToastHelmi.GrandTheftMinecart.Util.MetaDataManager;
-import org.bukkit.Bukkit;
 import org.bukkit.craftbukkit.v1_7_R2.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
 import org.bukkit.entity.Zombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
 
-/**
- *
- * @author Robin
- */
 public class CrackshotListener implements Listener {
 
     public CrimeManager m;
@@ -33,32 +26,6 @@ public class CrackshotListener implements Listener {
 
     @EventHandler
     public void hurtEntity(WeaponDamageEntityEvent e) {
-        if (e.getDamager() instanceof Player) {
-
-            final Player damager = (Player) e.getPlayer();
-
-            if (e.getVictim() instanceof Player && damager != e.getVictim()) {
-                final Player player = ((Player) e.getVictim());
-
-                int level;
-                try {
-                    level = MetaDataManager.getPlayerMetaData(Integer.class, damager, StaticMetaDataValue.WANTEDLEVEL);
-                } catch (Exception ex) {
-                    level = 0;
-                }
-                if (level <= 0) {
-                    m.setWantedLevel(damager, 1);
-                } else if (level == 1 && ((CraftPlayer) player).getHealth() <= e.getDamage()) {
-                    m.setWantedLevel(damager, 2);
-                    m.incrementInfringement(damager);
-                }
-            } else if (e.getVictim() instanceof Zombie
-                    || e.getVictim() instanceof Skeleton) {
-
-                m.setWantedLevel(damager, 3);
-                m.incrementInfringement(damager);
-            }
-
-        }
+        CrimeListener.hurtEntity(e.getPlayer(), e.getVictim(), e.getDamage());
     }
 }
--- src/main/java/me/ToastHelmi/GrandTheftMinecart/Police/Listener/CrimeListener.java
+++ src/main/java/me/ToastHelmi/GrandTheftMinecart/Police/Listener/CrimeListener.java
@@ -4,8 +4,8 @@ import com.shampaggon.crackshot.events.WeaponDamageEntityEvent;
 import me.ToastHelmi.GrandTheftMinecart.Police.CrimeManager;
 import me.ToastHelmi.GrandTheftMinecart.StaticValues.StaticMetaDataValue;
 import me.ToastHelmi.GrandTheftMinecart.Util.MetaDataManager;
-
 import org.bukkit.craftbukkit.v1_7_R2.entity.CraftPlayer;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.Horse;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Skeleton;
@@ -17,60 +17,57 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.vehicle.VehicleEnterEvent;
 
-public class CrimeListener implements Listener{
+public class CrimeListener implements Listener {
 
-	public CrimeManager m;
-	
-	public CrimeListener(){
-		m = CrimeManager.getInstance();
+    public CrimeManager m;
+
+    public CrimeListener() {
+        m = CrimeManager.getInstance();
+    }
+
+    public static void hurtEntity(Entity edamager, Entity victim, double damage) {
+        if (!(edamager instanceof Player)) {
+            return;
         }
-	
-	@EventHandler
-	public void hurtEntity(EntityDamageByEntityEvent e){
-		if(e.getDamager() instanceof Player ){
-			
-			final Player damager = (Player) e.getDamager();
-			
-			if(e.getEntity() instanceof Player && e.getDamager() != e.getEntity()){
-				final Player player = ((Player)e.getEntity());
-			
-				int level;
-				try{
-					level = MetaDataManager.getPlayerMetaData(Integer.class, damager, StaticMetaDataValue.WANTEDLEVEL);
-				}catch(Exception ex){
-					level = 0;
-				}
-				if(level <= 0){
-					m.setWantedLevel(damager, 1);
-				}
-				else if (level == 1 && ((CraftPlayer)player).getHealth() <= e.getDamage()){
-					m.setWantedLevel(damager, 2);
-					m.incrementInfringement(damager);
-				}
-			}
-			else if (e.getEntity() instanceof Zombie ||
-					 e.getEntity() instanceof Skeleton){
-				
-				m.setWantedLevel(damager, 3);
-				m.incrementInfringement(damager);
-			}
-			
-			
-		}
-	}
-	
-	@EventHandler
-	public void onHorseTheft(VehicleEnterEvent e){
-		if(e.getVehicle() instanceof Horse && e.getEntered() instanceof Player){
-			final Horse h = (Horse) e.getVehicle();
-			if(h.getOwner() != null && h.getOwner() != e.getEntered()){
-				if(m.getWantedLevel((Player) e.getEntered()) < 2){
-					m.setWantedLevel((Player)e.getEntered(), 2);
-					m.incrementInfringement((Player) e.getEntered());
-				}
-			}
-		}
-	}
-	
+        Player damager = (Player)edamager;
+        if (victim instanceof Player && damager != victim) {
+            final Player player = (Player) victim;
 
+            int level;
+            try {
+                level = MetaDataManager.getPlayerMetaData(Integer.class, damager, StaticMetaDataValue.WANTEDLEVEL);
+            } catch (Exception ex) {
+                level = 0;
+            }
+            if (level <= 0) {
+                CrimeManager.getInstance().setWantedLevel(damager, 1);
+            } else if (level == 1 && ((CraftPlayer) player).getHealth() <= damage) {
+                CrimeManager.getInstance().setWantedLevel(damager, 2);
+                CrimeManager.getInstance().incrementInfringement(damager);
+            }
+        } else if (victim instanceof Zombie
+                || victim instanceof Skeleton) {
+
+            CrimeManager.getInstance().setWantedLevel(damager, 3);
+            CrimeManager.getInstance().incrementInfringement(damager);
+        }
+    }
+
+    @EventHandler
+    public void hurtEntity(EntityDamageByEntityEvent e) {
+        hurtEntity(e.getDamager(), e.getEntity(), e.getDamage());
+    }
+
+    @EventHandler
+    public void onHorseTheft(VehicleEnterEvent e) {
+        if (e.getVehicle() instanceof Horse && e.getEntered() instanceof Player) {
+            final Horse h = (Horse) e.getVehicle();
+            if (h.getOwner() != null && h.getOwner() != e.getEntered()) {
+                if (m.getWantedLevel((Player) e.getEntered()) < 2) {
+                    m.setWantedLevel((Player) e.getEntered(), 2);
+                    m.incrementInfringement((Player) e.getEntered());
+                }
+            }
+        }
+    }
 }
