40eb6f504a7e2ec15643fe1063f4b87c846ec62e
roblabla <robinlambertz.dev@gmail.com>
2014-06-01 23:14:46 +0200

    Herp derp, wtf is up with that import

--- src/main/java/me/ToastHelmi/GrandTheftMinecart/Police/Listener/CrimeListener.java
+++ src/main/java/me/ToastHelmi/GrandTheftMinecart/Police/Listener/CrimeListener.java
@@ -1,13 +1,15 @@
 package me.ToastHelmi.GrandTheftMinecart.Police.Listener;
 
-import com.sun.scenario.Settings;
+import me.ToastHelmi.GrandTheftMinecart.GrandTheftMinecart;
 import me.ToastHelmi.GrandTheftMinecart.NMS.Police.PoliceOfficer;
 import me.ToastHelmi.GrandTheftMinecart.NMS.Police.Sniper;
 import me.ToastHelmi.GrandTheftMinecart.NMS.Police.Swat;
 import me.ToastHelmi.GrandTheftMinecart.Police.CrimeManager;
+import me.ToastHelmi.GrandTheftMinecart.Settings.Settings;
 import me.ToastHelmi.GrandTheftMinecart.StaticValues.SettingPath;
 import me.ToastHelmi.GrandTheftMinecart.StaticValues.StaticMetaDataValue;
 import me.ToastHelmi.GrandTheftMinecart.Util.MetaDataManager;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.craftbukkit.v1_7_R2.entity.CraftEntity;
 import org.bukkit.entity.Damageable;
 import org.bukkit.entity.Entity;
@@ -38,16 +40,16 @@ public class CrimeListener implements Listener {
         } catch (Exception ex) {
             level = 0;
         }
-
+        FileConfiguration config = GrandTheftMinecart.getInstance().getConfig();
         if (victim instanceof Player && damager != victim) {
             final Player player = (Player) victim;
 
             if (player.getHealth() <= damage) {
-                level = Math.max(level, Settings.getInt(SettingPath.CONDITION_KILL_PLAYER, 2));
+                level = Math.max(level, config.getInt(SettingPath.CONDITION_KILL_PLAYER, 2));
                 CrimeManager.getInstance().setWantedLevel(damager, level);
                 CrimeManager.getInstance().incrementInfringement(damager);
             } else {
-                level = Math.max(level, Settings.getInt(SettingPath.CONDITION_ATTACK_PLAYER, 1));
+                level = Math.max(level, config.getInt(SettingPath.CONDITION_ATTACK_PLAYER, 1));
                 CrimeManager.getInstance().setWantedLevel(damager, level);
             }
         } else if (handle instanceof PoliceOfficer
@@ -55,20 +57,20 @@ public class CrimeListener implements Listener {
                 || handle instanceof Sniper) {
 
             if (((Damageable) victim).getHealth() <= damage) {
-                level = Math.max(level, Settings.getInt(SettingPath.CONDITION_KILL_POLICE, 4));
+                level = Math.max(level, config.getInt(SettingPath.CONDITION_KILL_POLICE, 4));
                 CrimeManager.getInstance().setWantedLevel(damager, level);
                 CrimeManager.getInstance().incrementInfringement(damager);
             } else {
-                level = Math.max(level, Settings.getInt(SettingPath.CONDITION_ATTACK_POLICE, 3));
+                level = Math.max(level, config.getInt(SettingPath.CONDITION_ATTACK_POLICE, 3));
                 CrimeManager.getInstance().setWantedLevel(damager, level);
             }
         } else {
             if (((Damageable) victim).getHealth() <= damage) {
-                level = Math.max(level, Settings.getInt(SettingPath.CONDITION_KILL_MOB, 0));
+                level = Math.max(level, config.getInt(SettingPath.CONDITION_KILL_MOB, 0));
                 CrimeManager.getInstance().setWantedLevel(damager, level);
                 CrimeManager.getInstance().incrementInfringement(damager);
             } else {
-                level = Math.max(level, Settings.getInt(SettingPath.CONDITION_ATTACK_MOB, 0));
+                level = Math.max(level, config.getInt(SettingPath.CONDITION_ATTACK_MOB, 0));
                 CrimeManager.getInstance().setWantedLevel(damager, level);
             }
         }
